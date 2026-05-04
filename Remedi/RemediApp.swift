ab615268b9574649519e9240ef81a26d2175b766
MarcGabarda <marcggab@gmail.com>
2026-05-05 00:08:21 +1000

    Initial Commit

---
+++ Remedi/RemediApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemediApp.swift
+//  Remedi
+//
+//  Created by Marc Gabarda on 5/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct RemediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
